e58cf9c15a3f43531c9067353a66d6b19d455e17
JAEYEON <jydragon9@gmail.com>
2025-04-03 10:13:56 +0900

    dao,dto 업데이트/회원가입jsp추가
    회원가입 시 DAO 예외 메서드 추가 / 멤버 테이블DTO 요소추가

--- src/main/java/model/testDTO.java
+++ src/main/java/model/testDTO.java
@@ -1,4 +1,34 @@
 package model;
 
 public class testDTO {
+    private String Id;          // 회원 고유 ID
+    private String password;  // 비밀번호
+    private String name;      // 회원 이름
+    private String nikname;   // 닉네임 (UNIQUE)
+    
+	public String getId() {
+		return Id;
+	}
+	public void setId(String Id) {
+		this.Id = Id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNikname() {
+		return nikname;
+	}
+	public void setNikname(String nikname) {
+		this.nikname = nikname;
+	}
+
 }
